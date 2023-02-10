267832651d594f98228ff0a5e9c89ee6f5709a44
Speckkatze <91845448+Speckkatze@users.noreply.github.com>
2023-02-10 10:04:56 +0100

    Update mapBotTestbranch.py

--- mapBotTestbranch.py
+++ mapBotTestbranch.py
@@ -2,18 +2,19 @@ import time
 import RPi.GPIO as GPIO
 
 
-# Define the pins for the DRV8825 stepper driver
+# Define the pins for the DRV8825 stepper driver (motor 1)
 dir_pin0 = 13
 step_pin0 = 19
 enable_pin0 = 12
 mode_pins0 = (16, 17, 20)
 
-# Define the pins for the DRV8825 stepper driver
+# Define the pins for the DRV8825 stepper driver (motor 2)
 dir_pin1 = 24
 step_pin1 = 18
 enable_pin1 = 4
 mode_pins1 = (21, 22, 27)
 
+# Define the delay between steps
 delay = 0.0005
 
 # Set the GPIO mode
@@ -21,49 +22,48 @@ GPIO.setmode(GPIO.BCM)
 
 
 # Set up the GPIO pins
+# motor 1
 GPIO.setup(dir_pin0, GPIO.OUT)
 GPIO.setup(step_pin0, GPIO.OUT)
 GPIO.setup(enable_pin0, GPIO.OUT)
 
+# motor 2
 GPIO.setup(dir_pin1, GPIO.OUT)
 GPIO.setup(step_pin1, GPIO.OUT)
 GPIO.setup(enable_pin1, GPIO.OUT)
-
 
 # Disable the stepper motor by setting the enable pin to low
 GPIO.output(enable_pin0, GPIO.LOW)
 GPIO.output(enable_pin1, GPIO.LOW)
 
 
-
-
+# Function to let motors spin in opposite direction for driving straight
 def driveStraight(dir, quartRots):
-    straightSteps = quartRots*50
-    delay = 0.0005
-    if dir == 0:
+    straightSteps = quartRots*50    # 200 steps = full rotation, 50 steps = 1/4 rotation
+    delay = 0.0005                  # smaller delay for faster turn rate
+    if dir == 0:    # motors direction set in different directions
         GPIO.output(dir_pin0, GPIO.LOW)
         GPIO.output(dir_pin1, GPIO.HIGH)
-    elif dir == 1:
+    elif dir == 1:  # motors direction set in opposite diretion from dir == 0
         GPIO.output(dir_pin0, GPIO.HIGH)
         GPIO.output(dir_pin1, GPIO.LOW)
     else:
         raise ValueError("dir not a correct value (0 for forwards, 1 for backwards)")
-    
+    # motors turn on
     GPIO.output(enable_pin0, GPIO.HIGH)
     GPIO.output(enable_pin1, GPIO.HIGH)
-
-    for i in range(straightSteps):
+    for i in range(straightSteps):  # motors go set amount of steps
         GPIO.output(step_pin0, GPIO.HIGH)
         GPIO.output(step_pin1, GPIO.HIGH)
-        time.sleep(delay)
+        time.sleep(delay)           # delay between steps
         GPIO.output(step_pin0, GPIO.LOW)
         GPIO.output(step_pin1, GPIO.LOW)
         time.sleep(delay)
-
     GPIO.output(enable_pin0, GPIO.LOW)
     GPIO.output(enable_pin1, GPIO.LOW)
+    
 
-def driveGay(turnDir, eighthRot):
+def driveTurn(turnDir, eighthRot):
     turnSteps = eighthRot*50
     delay = 0.001
     if turnDir == 0:
@@ -73,10 +73,10 @@ def driveGay(turnDir, eighthRot):
         GPIO.output(dir_pin0, GPIO.LOW)
         GPIO.output(dir_pin1, GPIO.LOW)
     else:
-        raise ValueError("dir not a correct value (0 for left turn, 1 for right turn)")
-
+        raise ValueError("turnDir not a correct value (0 for left turn, 1 for right turn)")
     GPIO.output(enable_pin0, GPIO.HIGH)
     GPIO.output(enable_pin1, GPIO.HIGH)
+    
     
     for i in range(turnSteps):
         GPIO.output(step_pin0, GPIO.HIGH)
@@ -91,76 +91,7 @@ def driveGay(turnDir, eighthRot):
 
 driveStraight(0, 40)
 time.sleep(0.5)
-driveGay(0, 20)
-
-
-
-
-"""
-# Set the direction of the stepper motor
-GPIO.output(dir_pin0, GPIO.LOW)
-GPIO.output(dir_pin1, GPIO.HIGH)
-
-# Define the number of steps for the stepper motor to rotate
-steps = 1000
-
-# Enable the stepper motor by setting the enable pin to high
-GPIO.output(enable_pin0, GPIO.HIGH)
-GPIO.output(enable_pin1, GPIO.HIGH)
-
-# Loop through the steps and control the stepper motor
-for i in range(steps):
-    GPIO.output(step_pin0, GPIO.HIGH)
-    GPIO.output(step_pin1, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(step_pin0, GPIO.LOW)
-    GPIO.output(step_pin1, GPIO.LOW)
-    time.sleep(delay)
-
-# Disable the stepper motor by setting the enable pin to low
-GPIO.output(enable_pin0, GPIO.LOW)
-GPIO.output(enable_pin1, GPIO.LOW)
-
-
-
-
-
-
-time.sleep(0.5)
-
-
-
-
-
-
-# Set the direction of the stepper motor
-GPIO.output(dir_pin0, GPIO.HIGH)
-GPIO.output(dir_pin1, GPIO.LOW)
-
-# Define the number of steps for the stepper motor to rotate
-steps = 1000
-
-# Define the delay time between steps
-delay = 0.0005
-
-# Enable the stepper motor by setting the enable pin to high
-GPIO.output(enable_pin0, GPIO.HIGH)
-GPIO.output(enable_pin1, GPIO.HIGH)
-
-# Loop through the steps and control the stepper motor
-for i in range(steps):
-    GPIO.output(step_pin0, GPIO.HIGH)
-    GPIO.output(step_pin1, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(step_pin0, GPIO.LOW)
-    GPIO.output(step_pin1, GPIO.LOW)
-    time.sleep(delay)
-
-# Disable the stepper motor by setting the enable pin to low
-GPIO.output(enable_pin0, GPIO.LOW)
-GPIO.output(enable_pin1, GPIO.LOW)
-"""
-
+driveTurn(0, 20)
 
 
 
